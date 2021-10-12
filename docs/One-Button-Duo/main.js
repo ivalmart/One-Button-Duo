1c37ef53093560bae1c395557e8a4434adeb6d18
Ivan Martinez-Arias <50786762+UBSNASTy@users.noreply.github.com>
2021-10-12 16:26:09 -0700

    Pushed New Project into docs

---
+++ docs/One-Button-Duo/main.js
@@ -0,0 +1,17 @@
+// CMPM 170 One Button Duo Project
+// Robert Radzville
+// Ivan Martinez-Arias
+
+title = "Duo Game";
+
+description = `
+`;
+
+characters = [];
+
+options = {};
+
+function update() {
+  if (!ticks) {
+  }
+}
