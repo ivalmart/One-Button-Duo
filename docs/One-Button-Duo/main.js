a986d9da5e4bd278bb923641d4b032304969ee0b
Robert Radzville <rradzvil@ucsc.edu>
2021-10-12 17:11:36 -0700

    Added sprites, shape class

--- docs/One-Button-Duo/main.js
+++ docs/One-Button-Duo/main.js
@@ -7,9 +7,73 @@ title = "Duo Game";
 description = `
 `;
 
-characters = [];
+characters = [
+  `
+  PPPPP
+  P   P
+  P   P
+  P   P
+  PPPPP
+  `
+  ,`
+  B   B
+   B B
+    B
+   B B
+  B   B
+  `,
+  `
+    G
+   GGG
+   G G
+  GG GG
+  GGGGG
+  `,
+  `
+   RRR
+  R   R
+  R   R 
+  R   R
+   RRR  
+  `
+];
 
 options = {};
+
+let shapeSpeed = 0.25;
+
+class shape {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.reset(); 
+  }
+
+  reset() {
+    // Decide the shape
+    let shapeNum = Math.floor(Math.random() * 4);
+    if (shapeNum == 0) {
+      this.shapeChar = 'b';
+    } else if (shapeNum == 1) {
+      this.shapeChar = 'c';
+    } else if (shapeNum == 2) {
+      this.shapeChar = 'd';
+    } else {
+      this.shapeChar = 'e';
+    }
+  }
+
+  move() {
+    this.y += shapeSpeed;
+    char(this.shapeChar, this.x, this.y);
+  }
+
+  checkScreen() {
+    if (this.y > 100) {
+      this.reset();
+    }
+  }
+}
 
 function update() {
   if (!ticks) {
